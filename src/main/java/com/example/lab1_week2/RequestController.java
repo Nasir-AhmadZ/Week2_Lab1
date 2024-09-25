447156728c57b7c75109f11072435ce556bcf155
Nasir Ahmad <G00419630@atu.ie>
2024-09-25 15:59:21 +0100

    calculator done?

--- src/main/java/com/example/lab1_week2/RequestController.java
+++ src/main/java/com/example/lab1_week2/RequestController.java
@@ -22,6 +22,38 @@ public class RequestController {
     {
         return name +" "+ age;
     }
-    
+
+    @GetMapping("/calculator")
+    public String calc(@RequestParam float num1, @RequestParam float num2, @RequestParam String operation)
+    {
+        float total=0;
+        switch (operation)
+        {
+            case "add":
+                total = num1+num2;
+                break;
+
+            case "multiply":
+                total = num1*num2;
+                break;
+
+            case "divide":
+                if (num2==0)
+                {
+                    System.out.println("cannot divide by 0");
+                    break;
+                }
+                total = num1/num2;
+                break;
+
+            case "subtract":
+                total = num1-num2;
+                break;
+
+            default: break;
+        }
+
+        return num1 + operation + num2+ "=" + total;
+    }
 
 }
